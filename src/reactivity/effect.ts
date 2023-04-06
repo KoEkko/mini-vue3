05dfc99213c22fcce7845db50a15dd77c45f9a77
Ekko <2251930460@qq.com>
2023-04-06 23:46:28 +0800

    effect & reactive

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,43 @@
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn) {
+    this._fn = fn;
+  }
+
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+}
+
+const targetMap = new WeakMap();
+export function track(target, key) {
+  // target -> key -> dep
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target)
+  let deps = depsMap.get(key)
+  for (const effect of deps) {
+    effect.run()
+  }
+}
+
+let activeEffect;
+
+export function effect(fn) {
+  // fn
+  const _effect = new ReactiveEffect(fn);
+  return _effect.run.bind(_effect);
+}
