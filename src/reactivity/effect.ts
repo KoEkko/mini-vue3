cd039c08f4b9aafc66079a92035e5cfdfcc4da18
Ekko <2251930460@qq.com>
2023-04-07 10:47:25 +0800

    update stop & onStop fn

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,35 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  public scheduler: Function | undefined;
+  constructor(fn, scheduler?: Function) {
     this._fn = fn;
+    this.scheduler = scheduler;
   }
 
   run() {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
 const targetMap = new WeakMap();
@@ -23,21 +45,36 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+  if (!activeEffect) return;
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
-  let depsMap = targetMap.get(target)
-  let deps = depsMap.get(key)
+  let depsMap = targetMap.get(target);
+  let deps = depsMap.get(key);
   for (const effect of deps) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 }
 
 let activeEffect;
 
-export function effect(fn) {
+export function effect(fn, options: any = {}) {
   // fn
-  const _effect = new ReactiveEffect(fn);
-  return _effect.run.bind(_effect);
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  // options
+  extend(_effect, options);
+  _effect.run();
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
