e3c5fc3d589935d3d0271bb38f66b7dd7169fbca
Ekko <2251930460@qq.com>
2023-04-19 16:04:36 +0800

    delete packages

--- packages/runtime-core/src/componentEmit.ts
+++
@@ -1,8 +0,0 @@
-import { toHandlerKey } from "../shared/index";
-
-export function emit(instance, event: string, ...args) {
-  const { props } = instance;
-
-  const handler = props[toHandlerKey(event)];
-  handler && handler(...args);
-}
--- packages/runtime-core/src/componentProps.ts
+++
@@ -1,6 +0,0 @@
-export function initProps(instance, rawProps) {
-
-  instance.props = rawProps || {}
-  
-  // attrs
-}
